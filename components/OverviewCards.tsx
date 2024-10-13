034923c9a1e299708c5dc4748ba13205a83b5461
03upendra <upendrabhoyar123@gmail.com>
2024-10-13 20:35:02 +0530

    resolve error

--- components/OverviewCards.tsx
+++ components/OverviewCards.tsx
@@ -1,10 +1,22 @@
 import { Space, Statistic } from "antd";
+import { ReactNode } from "react";
 
-export default function OverviewCards({title,value,icon}:{title:string,value:number, icon:any}) {
+export default function OverviewCards({
+  title,
+  value,
+  icon,
+}: {
+  title: string;
+  value: number;
+  icon: ReactNode;
+}) {
   return (
-    <Space direction="horizontal" className="border border-gray-400 rounded-md p-2 pr-3 ">
-    {icon}
-    <Statistic title={title} value={value} />
-  </Space>
-  )
+    <Space
+      direction="horizontal"
+      className="border border-gray-400 rounded-md p-2 pr-3 "
+    >
+      {icon}
+      <Statistic title={title} value={value} />
+    </Space>
+  );
 }
